85fefb7112c314081d3868139defdc3cc02d44c2
Kabir Khan <168872491+mkaabirkhan@users.noreply.github.com>
2024-05-17 19:01:24 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt
+([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  {message:"Enter second number",type:"number",name:"secondNumber"},
+  {
+    message:"select one of the operators to perform operation",
+    type:"list",
+    name:"operator",
+    choices:["addition","subtraction","multipication","Division"]
+  },
+]);
+
+console.log(answer);
+ //Conditional statement
+
+ //if (answer.operator === "Addition"){
+ // console.log("your value is" + answer.firstNumber + answer.secondNumber);
+ //}
+
+ if (answer.operator === "addition") {
+  console.log(answer.firstNumber+answer.secondNumber);
+ }
+
+ else if (answer.operator==="subtraction"){
+  console.log(answer.firstNumber-answer.secondNumber);
+ }
+ 
+ else if (answer.operator==="multipication"){
+  console.log(answer.firstNumber*answer.secondNumber);
+ }
+
+ else if (answer.operator==="Division"){
+  console.log(answer.firstNumber/answer.secondNumber);
+ }
+
+ else{
+  console.log("please select valid operator")
+ }
